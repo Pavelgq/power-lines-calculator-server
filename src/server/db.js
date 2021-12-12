708411a7475f31201dd515f6f6a927e983c46285
Pavel <pavelgq@yandex.ru>
2021-12-12 18:29:52 +0300

    fix: heruku config db and local config db

--- src/server/db.js
+++ src/server/db.js
@@ -1,25 +1,18 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
-const dbConfig = require('../config/db-config')
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+let dbConfig;
+console.log(process.env.DEV_MODE);
+if (process.env.DEV_MODE) {
+  dbConfig = require("../config/db-config");
+} else {
+  dbConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const pool = new Pool(dbConfig);
-
-// (async () => {
-//   const client = await pool.connect()
-//   try {
-//     const res = await pool.query(`select * from users`);
-//     console.log(res.rows[0])
-//   } finally {
-//     client.release()
-//   }
-// })().catch(e => console.log(e.stack))
-
 
 module.exports = pool;
