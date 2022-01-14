f7795c054bac965e0a38828f76708a4007acebda
Pavel <pavelgq@yandex.ru>
2022-01-14 18:34:40 +0300

    fix: create action bug

--- src/fields/action/action.controller.js
+++ src/fields/action/action.controller.js
@@ -10,10 +10,11 @@ class ActionControllers {
     try {
       const { client_id, type, data, project_name } = req.body;
       const { accept_key } = req;
-      console.log(req.body);
+      console.log(client_id, type, data, project_name);
       let dataPath = "";
       if (data) {
         dataPath = `${client_id}-${Date.now()}.json`;
+
         console.log(dataPath);
         await fs.writeFile(
           dataPath,
@@ -22,13 +23,12 @@ class ActionControllers {
             if (err) console.log("error", err);
           }
         );
-
-        await db.query(
-          `INSERT INTO action (client_id, type, path_to_data, accept_key, project_name) VALUES ('${client_id}', '${type}', '${dataPath}', '${accept_key}', '${project_name}');`
-        );
-
-        return res.json({ message: "Действие пользователя сохранено" });
       }
+      await db.query(
+        `INSERT INTO action (client_id, type, path_to_data, accept_key, project_name) VALUES ('${client_id}', '${type}', '${dataPath}', '${accept_key}', '${project_name}');`
+      );
+
+      return res.json({ message: "Действие пользователя сохранено" });
     } catch (error) {
       logger.error("action add: ", error);
       return res.status(400).json({ error });
