f87c45c32c913548bc9da915540804c75c32a0cc
Pavelgq <pavelgq@yandex.ru>
2022-05-25 17:26:20 +0300

    fix: changed request route

--- src/fields/client/client.controller.js
+++ src/fields/client/client.controller.js
@@ -144,12 +144,9 @@ class ClientController {
       if (!clientData.rowCount) {
         return res.status(400).json({ message: "Пользователь не найден" });
       }
-      const newClientData = req.body;
-
-      const payload = { ...clientData, ...newClientData };
 
       await db.query(
-        `UPDATE client SET accept = 'true' WHERE id = '${clientId}';`
+        `UPDATE client SET accept = 'false' WHERE id = '${clientId}';`
       );
       return res.json({ message: "Запрос успешно принят" });
     } catch (error) {
