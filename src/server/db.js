f2d7d11183d286329215e2af6304cfb542eb5d2c
Pavel <pavelgq@yandex.ru>
2022-06-02 23:04:46 +0300

    fix: dbConfig

--- src/server/db.js
+++ src/server/db.js
@@ -1,24 +1,24 @@
 const Pool = require("pg").Pool;
 
 let dbConfig;
-// if (process.env.DEV_MODE == 1) {
-console.log("local");
-dbConfig = {
-  host: "localhost",
-  port: 5432,
-  database: "newdatabase",
-  user: "pavelgordeev",
-  password: process.env.DB_PASSWORD,
-};
-// } else {
-//   console.log("heroku");
-//   dbConfig = {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   };
-// }
+if (process.env.DEV_MODE == 1) {
+  console.log("local");
+  dbConfig = {
+    host: "localhost",
+    port: 5432,
+    database: "newdatabase",
+    user: "pavelgordeev",
+    password: process.env.DB_PASSWORD,
+  };
+} else {
+  console.log("heroku");
+  dbConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+}
 
 const pool = new Pool(dbConfig);
 
